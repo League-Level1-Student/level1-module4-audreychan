317597f6b37dc458c3c55e82f8771633f4ef6b6e
Idiot <league@WTS14-4.attlocal.net>
2018-11-25 11:30:16 -0800

    SimonSays, Jeopardy

--- src/Jeopardy.java
+++ src/Jeopardy.java
@@ -1,10 +1,8 @@
 
-
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2018
  *    Level 1
  */
-
 
 import java.applet.AudioClip;
 import java.awt.BorderLayout;
@@ -16,7 +14,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
 
-
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -27,14 +24,12 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-
-
 /* Check out the Jeopardy Handout to see what the end result should look like: http://bit.ly/1bvnvd4 */
 
 public class Jeopardy implements ActionListener {
 	private JButton firstButton;
 	private JButton secondButton;
-	private JButton thirdButton, fourthButton;
+	private JButton thirdButton, fourthButton, fifthButton;
 	private JPanel quizPanel;
 	private int score = 0;
 	private JLabel scoreBox = new JLabel("0");
@@ -52,38 +47,60 @@ public class Jeopardy implements ActionListener {
 		frame.setLayout(new BorderLayout());
 
 		// 1. Make the frame show up
+		frame.setVisible(true);
 
 		// 2. Give your frame a title
+		frame.setName("Jeopardy");
 
 		// 3. Create a JPanel variable to hold the header using the createHeader method
+		quizPanel.add(createHeader("Movies"));
 
 		// 4. Add the header component to the quizPanel
 
 		// 5. Add the quizPanel to the frame
+		frame.add(quizPanel);
 
 		// 6. Use the createButton method to set the value of firstButton
+		firstButton = createButton("$200");
 
 		// 7. Add the firstButton to the quizPanel
+		quizPanel.add(firstButton);
 
-		// 8. Write the code to complete the createButton() method below. Check that your
+		// 8. Write the code to complete the createButton() method below. Check that
+		// your
 		// game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
 
 		// 9. Use the secondButton variable to hold a button using the createButton
 		// method
+		secondButton = createButton("$400");
 
 		// 10. Add the secondButton to the quizPanel
+		quizPanel.add(secondButton);
 
 		// 11. Add action listeners to the buttons (2 lines of code)
+		firstButton.addActionListener(this);
+		secondButton.addActionListener(this);
 
 		// 12. Write the code to complete the actionPerformed() method below
 
 		// 13. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
-		
-		 /*
+		thirdButton = createButton("$600");
+		quizPanel.add(thirdButton);
+		thirdButton.addActionListener(this);
+
+		fourthButton = createButton("$800");
+		quizPanel.add(fourthButton);
+		fourthButton.addActionListener(this);
+
+		fifthButton = createButton("$1000");
+		quizPanel.add(fifthButton);
+		fifthButton.addActionListener(this);
+
+		/*
 		 * [optional] Use the showImage or playSound methods when the user answers a
 		 * question
-		 */		
-		
+		 */
+
 		frame.pack();
 		quizPanel.setLayout(new GridLayout(buttonCount + 1, 3));
 		frame.add(makeScorePanel(), BorderLayout.NORTH);
@@ -91,60 +108,99 @@ public class Jeopardy implements ActionListener {
 				Toolkit.getDefaultToolkit().getScreenSize().width);
 	}
 
-
 	private JButton createButton(String dollarAmount) {
-		
+
 		// Create a new JButton
+		JButton button = new JButton();
 
 		// Set the text of the button to the dollarAmount
+		button.setText(dollarAmount);
 
 		// Increment the buttonCount (this should make the layout vertical)
+		buttonCount++;
 
 		// Return your new button instead of the temporary button
 
-		return new JButton("temporary button");
+		return button;
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		
+
 		// Remove this temporary message after testing:
-		JOptionPane.showMessageDialog(null, "pressed " + ((JButton) e.getSource()).getText() + " button");
 
-		JButton buttonPressed = (JButton) e.getSource();
 		// If the buttonPressed was the firstButton
+		JButton pressed = (JButton) e.getSource();
+		if (pressed == firstButton) {
+			askQuestion("", "", 200);
+			firstButton.setText("");
+		}
+		// Call the askQuestion() method
 
-			// Call the askQuestion() method
- 
-		// Complete the code in the askQuestion() method. When you play the game, the score should change.
+		// Complete the code in the askQuestion() method. When you play the game, the
+		// score should change.
 
 		// If the buttonPressed was the secondButton
+		if (pressed == secondButton) {
+			askQuestion("", "", 400);
+			secondButton.setText("");
+		}
 
-			// Call the askQuestion() method with a harder question
+		if (pressed == thirdButton) {
+			askQuestion("", "", 600);
+			thirdButton.setText("");
+		}
 
-		// Clear the text on the button that was pressed (set the button text to nothing)
+		if (pressed == fourthButton) {
+			askQuestion("", "", 800);
+			fourthButton.setText("");
+		}
+
+		if (pressed == fifthButton) {
+			askQuestion("", "", 1000);
+			fifthButton.setText("");
+		}
+		// Call the askQuestion() method with a harder question
+
+		// Clear the text on the button that was pressed (set the button text to
+		// nothing)
 
 	}
 
 	private void askQuestion(String question, String correctAnswer, int prizeMoney) {
-		
-		// Use the playJeopardyTheme() method to play music while the use thinks of an answer
-		
-		// Remove this temporary message and replace it with a pop-up that asks the user the question
-		JOptionPane.showMessageDialog(null, "this is where the question will be asked");
-		
-		// Stop the theme music when they have entered their response. Hint: use the sound variable 
-		
+
+		// Use the playJeopardyTheme() method to play music while the use thinks of an
+		// answer
+		playJeopardyTheme();
+
+		// Remove this temporary message and replace it with a pop-up that asks the user
+		// the question
+		String userAnswer = JOptionPane.showInputDialog(question);
+
+		// Stop the theme music when they have entered their response. Hint: use the
+		// sound variable
+		sound.stop();
+
 		// If the answer is correct
+		if (userAnswer.equals(correctAnswer)) {
+			score += prizeMoney;
+			JOptionPane.showMessageDialog(null, "Correct!");
+		} else {
+			score -= prizeMoney;
+			JOptionPane.showMessageDialog(null, "Sorry, incorrect.");
+		}
 
-			// Increase the score by the prizeMoney
+		updateScore();
 
-			// Pop up a message to tell the user they were correct
+		// Increase the score by the prizeMoney
+
+		// Pop up a message to tell the user they were correct
 
 		// Otherwise
 
-			// Decrement the score by the prizeMoney
+		// Decrement the score by the prizeMoney
 
-			// Pop up a message to tell the user they were wrong and give them the correct answer
+		// Pop up a message to tell the user they were wrong and give them the correct
+		// answer
 
 		// Call the updateScore() method
 
@@ -199,3 +255,12 @@ public class Jeopardy implements ActionListener {
 		frame.pack();
 	}
 }
+
+/*
+ * 5 Movie questions and answers: Longest Harry Potter movie? Chamber of Secrets
+ * (2:54) Most Popular quote from Princess Leia from A New Hope?
+ * "Help me Obi Wan Kenobi, you're my only hope." Who are the 3 Chrises that
+ * play in the Marvel Movies? Hemsworth, Evans, Pratt What is the latest MCU
+ * movie? Ant man and the wasp
+ * 
+ */
--- src/SimonSays.java
+++ src/SimonSays.java
@@ -1,37 +1,39 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2018
  *    Level 1
  */
 
 import java.awt.Component;
-import java.awt.Frame;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.net.URL;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Random;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import java.io.IOException;
 
 public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
 	JFrame frame;
-	
+
 	HashMap<Integer, String> images = new HashMap<Integer, String>();
 	private int imageIndex;
 	private int tries = 0;
 	private boolean simonSays = false;
 	Date timeAtStart;
+	int turns = 0;
+	int points = 0;
 
 	private void makeAlbum() {
-		// 2. Add the four images that match keyboard keys like this: 
+		// 2. Add the four images that match keyboard keys like this:
 		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
 		images.put(new Integer(KeyEvent.VK_DOWN), "down.jpg");
 		images.put(new Integer(KeyEvent.VK_LEFT), "left.jpg");
@@ -41,7 +43,7 @@ public class SimonSays extends KeyAdapter {
 		// 'Simon says' otherwise press a different key"
 		JOptionPane window = new JOptionPane();
 		window.setMessage("Press the matching key when 'Simon says' otherwise press a different key");
-		
+
 		// 4. Call the showImage method to show an image
 		showImage();
 	}
@@ -50,34 +52,73 @@ public class SimonSays extends KeyAdapter {
 		// 15. Make a points variable to track the score.
 
 		// 16. If the keyCode matches the imageIndex and "Simon says"
-		
-			// 17. Increase the value of score
-		
-			// 18. Use the speak method to tell the user they were correct
-		
+		if (e.getKeyCode() == imageIndex && simonSays) {
+			points++;
+			speak("Correct");
+			showImage();
+			tries = 0;
+		}
+
+		// 17. Increase the value of score
+
+		// 18. Use the speak method to tell the user they were correct
+
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't say..."
-		
-			// 20.  Increase the value of score
-		
-			// 21. Use the speak method to tell the user they were correct
-		
+
+		else if (e.getKeyCode() != imageIndex && simonSays == false) {
+			points++;
+			speak("Correct");
+			showImage();
+			tries = 0;
+		}
+
+		// 20. Increase the value of score
+
+		// 21. Use the speak method to tell the user they were correct
+
 		// 22. Increment tries by 1
-		
+		else {
+			tries++;
+			speak("Incorrect");
+			points--;
+			showImage();
+		}
+
 		// 25. If tries is greater than 9 (or however many you want)...
-		
-			// 26. Tell the user their score
-		
-			// 27. Exit the program
+
+		/*
+		 * if (tries > 1) { points--; showImage(); tries = 0; }
+		 */
+
+		// 26. Tell the user their score
+
+		// 27. Exit the program
 
 		// 23. Dispose of the frame
 
 		// 24. Call the showImage method to show a new image
+
 	}
 
 	private void showImage() {
+		turns++;
+		if (turns > 5) {
+			if (points == 1) {
+				speak("Game over. You got " + points + " point.");
+				System.out.println("Game over. You got " + points + " point.");
+			} else if (points == -1) {
+				speak("Game over. You got " + points + " point.");
+				System.out.println("Game over. You got " + points + " point.");
+			} else {
+				speak("Game over. You got " + points + " points.");
+				System.out.println("Game over. You got " + points + " points.");
+			}
+			System.exit(0);
+		}
+
 		// 5. Initialize your frame to a new JFrame()
 		frame = new JFrame();
-		
+
 		// 6. Set the frame to visible
 		frame.setVisible(true);
 
@@ -86,13 +127,13 @@ public class SimonSays extends KeyAdapter {
 
 		// 8. Set the name of your frame
 		frame.setName("Simon Says");
-		
+
 		// 9. Pack the frame
 		frame.pack();
-		
+
 		// 10. Set the defaultCloseOperation of your from to JFrame.EXIT_ON_CLOSE
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		// 11. Add a key listener to the frame
 		frame.addKeyListener(this);
 
@@ -100,16 +141,17 @@ public class SimonSays extends KeyAdapter {
 		Random gen = new Random();
 		int rand = gen.nextInt(2);
 
-		// 13. Use the Random and the speak method to either say 
+		// 13. Use the Random and the speak method to either say
 		// "Simon says press this key" or "Press this key"
-		if(rand == 0) {
+		if (rand == 0) {
 			speak("Simon says press this key");
+			simonSays = true;
+		} else if (rand == 1) {
+			speak("Press this key");
+			simonSays = false;
 		}
-		
 
 		// 14. Above, set the value of simonSays to true/false appropriately
-
-		
 	}
 
 	private Component getNextRandomImage() {
@@ -137,13 +179,10 @@ public class SimonSays extends KeyAdapter {
 }
 
 /*
- * BONUS!
- * Add a timer :
- * ~~~ where the code starts running ~~~ 
- * timeAtStart = newDate();
+ * BONUS! Add a timer : ~~~ where the code starts running ~~~ timeAtStart =
+ * newDate();
  *
- * ~~~ where the code ends ~~~ 
- * Date timeAtEnd = new Date();
+ * ~~~ where the code ends ~~~ Date timeAtEnd = new Date();
  * System.out.println((timeAtEnd.getTime()-timeAtStart.getTime())/1000);
  * System.exit(0);
  */
